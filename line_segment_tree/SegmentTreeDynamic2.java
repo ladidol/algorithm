8ecac81c5b75df60c835e008f28580021346509f
ladidol <599426945@qq.com>
2022-07-22 17:44:58 +0800

    线段树 小修改

--- line_segment_tree/SegmentTreeDynamic2.java
+++ line_segment_tree/SegmentTreeDynamic2.java
@@ -8,14 +8,14 @@ package line_segment_tree;
 public class SegmentTreeDynamic2 {
     class Node {
         Node left, right;
-        int val, add;
+        int val, lazy;
     }
     private int N = (int) 1e9;
     private Node root = new Node();
     public void update(Node node, int start, int end, int l, int r, int val) {
         if (l <= start && end <= r) {
-            node.val += (end - start + 1) * val;
-            node.add += val;
+            node.val += (end - start + 1) * val;//这里根据需求变化 主要是是 自加或者等号或者去掉个数
+            node.lazy += val;
             return ;
         }
         int mid = (start + end) >> 1;
@@ -28,22 +28,22 @@ public class SegmentTreeDynamic2 {
         if (l <= start && end <= r) return node.val;
         int mid = (start + end) >> 1, ans = 0;
         pushDown(node, mid - start + 1, end - mid);
-        if (l <= mid) ans += query(node.left, start, mid, l, r);
-        if (r > mid) ans += query(node.right, mid + 1, end, l, r);
+        if (l <= mid) ans += query(node.left, start, mid, l, r);//这里根据需求变化 主要是是 自加或者等号
+        if (r > mid) ans += query(node.right, mid + 1, end, l, r);//有时候是Math.max();
         return ans;
     }
     private void pushUp(Node node) {
-        node.val = node.left.val + node.right.val;
+        node.val = node.left.val + node.right.val;//有时候是Math.max();
     }
     private void pushDown(Node node, int leftNum, int rightNum) {
         if (node.left == null) node.left = new Node();
         if (node.right == null) node.right = new Node();
-        if (node.add == 0) return ;
-        node.left.val += node.add * leftNum;
-        node.right.val += node.add * rightNum;
+        if (node.lazy == 0) return ;
+        node.left.val += node.lazy * leftNum;//这里根据需求变化 主要是是 自加或者等号
+        node.right.val += node.lazy * rightNum;
         // 对区间进行「加减」的更新操作，下推懒惰标记时需要累加起来，不能直接覆盖
-        node.left.add += node.add;
-        node.right.add += node.add;
-        node.add = 0;
+        node.left.lazy += node.lazy;
+        node.right.lazy += node.lazy;
+        node.lazy = 0;
     }
 }
